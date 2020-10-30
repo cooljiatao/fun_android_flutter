41de4b08ec48442b51a7f52d239aa46563656579
漂流瓶 <535318720@qq.com>
2020-10-30 18:28:59 +0800

    main 包名冲突

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart' hide Router;
+import 'package:flutter/material.dart' hide Router;
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
